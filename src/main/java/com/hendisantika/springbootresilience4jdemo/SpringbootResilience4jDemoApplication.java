4c1003735e39271661ec8ca3bccff309df6233f9
Hendi Santika <hendisantika@gmail.com>
2020-02-23 15:48:03 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootresilience4jdemo/SpringbootResilience4jDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootresilience4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootResilience4jDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootResilience4jDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootresilience4jdemo/SpringbootResilience4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootresilience4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootResilience4jDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
