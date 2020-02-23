085f34bacd81818f851b3d6a71647fc4400c2cf8
Hendi Santika <hendisantika@gmail.com>
2020-02-23 15:50:56 +0700

    #5 - Add Consumer in Main App

--- src/main/java/com/hendisantika/springbootresilience4jdemo/SpringbootResilience4jDemoApplication.java
+++ src/main/java/com/hendisantika/springbootresilience4jdemo/SpringbootResilience4jDemoApplication.java
@@ -1,11 +1,15 @@
 package com.hendisantika.springbootresilience4jdemo;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerOpenException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
+
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 @Log4j2
@@ -14,6 +18,16 @@ public class SpringbootResilience4jDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootResilience4jDemoApplication.class, args);
     }
+
+    private static final Consumer<Runnable> consumer = runnable -> IntStream.range(0, 5).forEach(value -> {
+        try {
+            runnable.run();
+        } catch (CircuitBreakerOpenException e) {
+            log.warn("Circuit breaker applied");
+        } catch (Exception e) {
+            log.warn("Exception in method");
+        }
+    });
 
     @Service
     class CircuitBreakerTestService {
