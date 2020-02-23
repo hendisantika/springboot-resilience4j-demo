a97387413c78eff1f338146e29049b0fe4a247a2
Hendi Santika <hendisantika@gmail.com>
2020-02-23 15:51:41 +0700

    #7 - Add applicationRunner in Main App

--- src/main/java/com/hendisantika/springbootresilience4jdemo/SpringbootResilience4jDemoApplication.java
+++ src/main/java/com/hendisantika/springbootresilience4jdemo/SpringbootResilience4jDemoApplication.java
@@ -4,6 +4,7 @@ import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerOpenException;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +38,23 @@ public class SpringbootResilience4jDemoApplication {
         return circuitBreakerRegistry.circuitBreaker("default");
     }
 
+    @Bean
+    ApplicationRunner applicationRunner(CircuitBreakerTestService circuitBreakerTestService,
+                                        CircuitBreaker defaultCircuitBreaker) {
+        return applicationArguments -> {
+
+            log.info("Running without circuit breaker ...");
+            consumer.accept(() -> circuitBreakerTestService.timeout());
+
+            log.info("Running with circuit breaker using annotations ...");
+            consumer.accept(() -> circuitBreakerTestService.timeoutWithCircuitBreaker());
+
+            log.info("Running with default circuit breaker using manual invocation ...");
+            Runnable decoratedRunnable = CircuitBreaker.decorateRunnable(defaultCircuitBreaker,
+                    () -> circuitBreakerTestService.timeout());
+            consumer.accept(() -> decoratedRunnable.run());
+        };
+    }
 
     @Service
     class CircuitBreakerTestService {
